7f6525af12ffc4ebbc0ce763eedbbecc091b99a8
GM <mrgavintech@gmail.com>
2020-03-29 09:39:57 -0500

    feat: add initial structure

---
+++ __main__.py
@@ -0,0 +1,6 @@
+from investor_tools import app
+
+if __name__ == "__main__":
+    app.run()
+
+    # examples https://dev.to/codemouse92/dead-simple-python-project-structure-and-imports-38c6
---
+++ investor_tools/app.py
@@ -0,0 +1,6 @@
+from investor_tools.calculate import blah
+
+
+def run():
+    blah();
+    print('It\'s running')
